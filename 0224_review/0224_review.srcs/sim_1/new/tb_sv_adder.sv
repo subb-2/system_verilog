105f1a7bd39146167478f6177d2929a6c001a627
subb-2 <zhsks312@gmail.com>
2026-02-24 21:00:56 +0900

    update sv adder

--- 0224_review/0224_review.srcs/sim_1/new/tb_sv_adder.sv
+++ 0224_review/0224_review.srcs/sim_1/new/tb_sv_adder.sv
@@ -22,44 +22,156 @@ endclass  //transaction
 class generator;
     //randomrize를 위해 random 값 가져와야 함 
     transaction tr;
-    virtual adder_interface adder_interf_gen;
+    mailbox #(transaction) gen2drv_mbox;
+    event gen_next_ev;
 
-    function new(virtual adder_interface adder_interf_ext);
-        this.adder_interf_gen = adder_interf_ext;
+    function new(mailbox#(transaction) gen2drv_mbox, event gen_next_ev);
+        this.gen2drv_mbox = gen2drv_mbox;
+        this.gen_next_ev = gen_next_ev;
     endfunction  //new()
 
-    task run();
-        tr = new();
-        tr.randomize();
-
-        adder_interf_gen.a = tr.a;
-        adder_interf_gen.b = tr.b;
-        adder_interf_gen.mode = tr.mode;
-
-        #10;
+    task run(int count);
+        repeat (count) begin
+            tr = new();
+            tr.randomize();
+            gen2drv_mbox.put(tr);
+            @(gen_next_ev);
+        end
 
     endtask  //run
 endclass  //generator
 
+class driver;
+
+    transaction tr;
+    virtual adder_interface adder_if;
+    mailbox #(transaction) gen2drv_mbox;
+    event mon_next_ev;
+
+    function new(mailbox#(transaction) gen2drv_mbox, event mon_next_ev,
+                virtual adder_interface adder_if);
+        this.gen2drv_mbox = gen2drv_mbox;
+        this.mon_next_ev = mon_next_ev;
+        this.adder_if = adder_if;
+    endfunction //new()
+
+    task run();
+        forever begin
+            gen2drv_mbox.get(tr);
+            adder_if.a = tr.a;
+            adder_if.b = tr.b;
+            adder_if.mode = tr.mode;
+            #10;
+            -> mon_next_ev;
+        end
+    endtask //run()
+endclass //driver
+
+class monitor;
+
+    transaction tr;
+    virtual adder_interface adder_if;
+    mailbox #(transaction) mon2scb_mbox;
+    event mon_next_ev; //gen이 발생 
+
+    function new(mailbox#(transaction) mon2scb_mbox, event mon_next_ev,
+                virtual adder_interface adder_if);
+        this.mon2scb_mbox = mon2scb_mbox;
+        this.mon_next_ev = mon_next_ev;
+        this.adder_if = adder_if;
+    endfunction //new()
+
+    task run ();
+        forever begin
+            @(mon_next_ev);
+            tr = new();
+            tr.a = adder_if.a;
+            tr.b = adder_if.b;
+            tr.mode = adder_if.mode;
+            tr.s = adder_if.s;
+            tr.c = adder_if.c;
+            mon2scb_mbox.put(tr);            
+        end
+
+    endtask //run
+endclass //monitor
+
+class scoreboard;
+
+    transaction tr;
+    mailbox #(transaction) mon2scb_mbox;
+    event gen_next_ev;
+
+    function new(mailbox#(transaction) mon2scb_mbox, event gen_next_ev);
+        this.mon2scb_mbox = mon2scb_mbox;
+        this.gen_next_ev = gen_next_ev;
+        
+    endfunction //new()
+
+    task run ();
+        forever begin
+            mon2scb_mbox.get(tr);
+            $display("%t : a = %d, b = %d, mode = %d, s = %d, c = %d", $time, tr.a, tr.b, tr.mode, tr.s, tr.c);
+            -> gen_next_ev;
+        end
+
+    endtask //run
+
+endclass //scoreboard
+
+class environment;
+
+    generator gen;
+    driver drv;
+    monitor mon;
+    scoreboard scb;
+
+    mailbox #(transaction) gen2drv_mbox;
+    mailbox #(transaction) mon2scb_mbox;
+
+    event gen_next_ev;
+    event mon_next_ev;
+
+    function new(virtual adder_interface adder_if);
+        gen2drv_mbox = new();
+        mon2scb_mbox = new();
+        gen = new(gen2drv_mbox, gen_next_ev);
+        drv = new(gen2drv_mbox, mon_next_ev, adder_if);
+        mon = new(mon2scb_mbox, mon_next_ev, adder_if);
+        scb = new(mon2scb_mbox, gen_next_ev);
+
+    endfunction //new()
+
+    task run();
+        fork
+            gen.run(10);
+            drv.run();
+            mon.run();
+            scb.run();
+        join_any
+
+        $stop;
+    endtask //run()
+endclass //environment
+
 module tb_sv_adder ();
 
 
-    adder_interface adder_interf ();
+    adder_interface adder_if ();
 
-    generator gen;
+    environment env;
 
     sv_adder dut (
-        .a(adder_interf.a),
-        .b(adder_interf.b),
-        .mode(adder_interf.mode),
-        .s(adder_interf.s),
-        .c(adder_interf.c)
+        .a(adder_if.a),
+        .b(adder_if.b),
+        .mode(adder_if.mode),
+        .s(adder_if.s),
+        .c(adder_if.c)
     );
 
     initial begin
-        gen = new(adder_interf);
-        gen.run();
-        $stop;
+        env = new(adder_if);
+        env.run();
     end
 
 endmodule
